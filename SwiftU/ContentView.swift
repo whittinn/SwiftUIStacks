3e39d76277bd9f717616d5d97c883c23880eb3e1
Nathaniel Whittington <whittinn@gmail.com>
2022-03-07 09:20:04 -0600

    Initial Commit

---
+++ SwiftU/ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  SwiftU
+//
+//  Created by Nathaniel Whittington on 3/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Spacer()
+            HStack{
+                Spacer()
+                Text("Swift-UI")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.pink)
+                Spacer()
+                Text("Swift-UI")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.pink)
+                Spacer()
+                Text("Swift-UI")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.pink)
+                Spacer()
+                
+            }
+            Spacer()
+            HStack{
+                Spacer()
+                Text("Swift-UI")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.pink)
+                Spacer()
+                Text("Swift-UI")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.pink)
+                Spacer()
+                Text("Swift-UI")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.pink)
+                Spacer()
+            }
+            Spacer()
+            HStack{
+                Spacer()
+                Text("Swift-UI")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.pink)
+                Spacer()
+                Text("Swift-UI")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.pink)
+                Spacer()
+                Text("Swift-UI")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.pink)
+                Spacer()
+            }
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftU/SwiftUApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUApp.swift
+//  SwiftU
+//
+//  Created by Nathaniel Whittington on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
